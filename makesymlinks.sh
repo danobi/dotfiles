edc043fb877ae018759c009a509ad0bfef388bd9
Daniel Xu <dlxu2@yahoo.com>
2015-11-14 18:22:59 -0600

    Fix makesymlinks.sh installing oh-my-zsh to wrong directory

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -4,12 +4,36 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
+# Entry point
+dir=~/dotfiles # dotfiles directory
+olddir=$dir/dotfiles_old # old dotfiles backup directory
+files="vimrc zshrc tmux.conf" # list of files/folders to symlink in homedir
+
+# create dotfiles_old in homedir
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "done"
+
+# change to the dotfiles directory
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file $olddir   #Cannot stat errors are ok, it means old dotfiles did not exist
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
+
 install_zsh () {
 # Test to see if zshell is installed. If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-		git clone http://github.com/robbyrussell/oh-my-zsh.git
+    if [[ ! -d ~/.oh-my-zsh/ ]]; then
+		git clone http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh/
 	fi
 	# Set the default shell to zsh if it isn't currently set to zsh
 	if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
@@ -40,29 +64,6 @@ else
 	fi
 fi
 }
-
-# Entry point
-dir=~/dotfiles # dotfiles directory
-olddir=$dir/dotfiles_old # old dotfiles backup directory
-files="vimrc zshrc tmux.conf" # list of files/folders to symlink in homedir
-
-# create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
-echo "done"
-
-# change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
-echo "done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file $olddir   #Cannot stat errors are ok, it means old dotfiles did not exist
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
-done
 
 # install and chsh to zsh
 install_zsh
