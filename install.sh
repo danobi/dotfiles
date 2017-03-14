5b184e2871715ba4fd4a304103df6ac85f234c48
Daniel Xu <dxu@dxuuu.xyz>
2017-03-13 20:55:50 -0500

    Use https for cloning

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ install_stuff () {
 
   # Let's clone some specific git repos
   if [[ ! -d ~/.oh-my-zsh/ ]]; then
-    git clone http://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh/
+    git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh/
   fi
   if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
