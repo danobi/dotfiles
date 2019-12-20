b1f4d86f81780de4de0ed043bb904b462751948b
Daniel Xu <dxu@dxuuu.xyz>
2019-12-20 12:13:10 -0800

    nvim: remove remnants

--- install.sh
+++ install.sh
@@ -29,17 +29,12 @@ install_stuff () {
   # Let's install some packages
   $PKG_MANAGER_CMD $DEFAULT_PKGS
 
-  # Create a nvim config directory so our plugins don't get put into
-  # stow's symlinked dirs
-  mkdir -p ~/.config/nvim/bundle
-
   # Let's clone some specific git repos
   if [[ ! -d ~/.oh-my-zsh/ ]]; then
     git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh/
   fi
   if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-    cp -r ~/.vim/bundle/Vundle.vim ~/.config/nvim/bundle
   fi
   if [[ ! -d ~/.tmux/plugins/tpm ]]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
