danobi/dotfiles

commit 7b9740b08413e59152f24f02a9d869585fafc824

vim: Fix syntax highlighting in large prr files

vim/.vimrc
@@ -101,6 +101,7 @@ augroup END
 "Built in settings
 fixdel                    "Sometimes hitting backspace in :command prints ^* instead
 syntax manual             "Syntax off by default, but allow per-buffer toggling
+syntax sync minlines=9999 "Making state-based syntax highlighting work for large files (used for prr)
 set nonu                  "Turn off line numbers; they're kind of useless anyways
 set ruler                 "Always shows info on bottom
 set cursorline            "Highlight current line

commit f97325c47ca2a69de0032292ba6cb67da2b1c21c

vim: Fix prr syntax highlighting (again)
I think the original config (see fixes tag) was being overwritten or
ignored when the augroup for prr kicked in. Setting the syntax-sync in
the augroup seems to work better.

Fixes: 7b9740b ("vim: Fix syntax highlighting in large prr files")

vim/.vimrc
@@ -72,6 +72,8 @@ augroup END
 augroup Prr
   autocmd!
   autocmd BufRead,BufNewFile *.prr set syntax=on
+  "Fixstate-based syntax highlighting for large files
+  autocmd BufRead,BufNewFile *.prr syntax sync fromstart
 
   "Make prr added/deleted highlighting more apparent
   autocmd BufRead,BufNewFile *.prr hi! link prrAdded Function
@@ -96,7 +98,6 @@ augroup END
 "Built in settings
 fixdel                    "Sometimes hitting backspace in :command prints ^* instead
 syntax manual             "Syntax off by default, but allow per-buffer toggling
-syntax sync minlines=9999 "Making state-based syntax highlighting work for large files (used for prr)
 set nonu                  "Turn off line numbers; they're kind of useless anyways
 set ruler                 "Always shows info on bottom
 set cursorline            "Highlight current line