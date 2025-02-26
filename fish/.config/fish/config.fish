a1c02568f67268227d4dc1a00a144bb52594e2cd
Daniel Xu <dxu@dxuuu.xyz>
2025-02-26 14:46:29 -0700

    fish: Add custom npm global dir to path
    Signed-off-by: Daniel Xu <dxu@dxuuu.xyz>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -27,6 +27,7 @@ if status is-interactive
   fish_add_path $HOME/.local/bin
   fish_add_path $HOME/.cargo/bin
   fish_add_path $HOME/go/bin
+  fish_add_path $HOME/.npm-global/bin
 
   # Connect to gnome keyring ssh-agent
   set (gnome-keyring-daemon --start 2>/dev/null | string split "=")
